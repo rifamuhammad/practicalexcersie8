8f330bca6cdbb057e0cd32a6dfa4439b0981ca44
rifamuhammad <62130549+rifamuhammad@users.noreply.github.com>
2024-03-01 20:25:03 +0700

    commit

---
+++ Object Repository/3. Preference Dependencies.rs
@@ -0,0 +1,11 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<MobileElementEntity>
+   <description></description>
+   <name>3. Preference Dependencies</name>
+   <tag></tag>
+   <elementGuidId>14d124f7-2eae-4555-9f34-8efc520d98e8</elementGuidId>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <locator></locator>
+   <locatorStrategy>ATTRIBUTES</locatorStrategy>
+</MobileElementEntity>
